dc5728ebb28237332efd131f937f2140e00246e7
gulnaz <janbosunova.kk@gmail.com>
2022-01-12 11:52:50 +0300

    first

---
+++ src/components/Comment.js
@@ -0,0 +1,10 @@
+function Comment (){
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar" src="" alt="" />
+                <div className="UserInfo-name">Kitty</div>
+            </div>
+        </div>
+    )
+}
